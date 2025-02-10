a56da93d8b8553c511eaa626be2446a3e988fdc4
peppedilillo <peppedilillo@gmail.com>
2025-02-10 11:31:14 +0100

    docker environments. basic media file handling.

---
+++ README.md
@@ -0,0 +1,13 @@
+For production:
+`docker compose -f compose.prod.yml build`
+`docker compose -f compose.prod.yml up`
+The website should be accessible at `http://localhost:1337/
+
+For development:
+`docker compose build`
+`docker compose up`
+The website should be accessible at `http://localhost:8000/`
+
+Based on [this](https://testdriven.io/blog/dockerizing-django-with-postgres-gunicorn-and-nginx/) guide.
+I still don't understand fully what the server reverse proxy settings are doing. 
+Note I expect them to interact with the django app setting `CSRF_TRUSTED_ORIGINS`.
---
+++ django/hhelm/configs/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.core.files.storage import FileSystemStorage
+from django.contrib.auth.decorators import login_required
+
+from hhelm.settings import MEDIA_ROOT, MEDIA_URL, DEBUG
+
+@login_required
+def image_upload(request):
+    if request.method == "POST" and request.FILES["image_file"]:
+        image_file = request.FILES["image_file"]
+        fs = FileSystemStorage()
+        filename = fs.save(image_file.name, image_file)
+        image_url = fs.url(filename)
+        return render(
+            request,
+            "configs/upload.html",
+            {"image_url": image_url},
+        )
+    return render(request, "configs/upload.html")
