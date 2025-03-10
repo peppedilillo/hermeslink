e91998088599e122fccaa623fe530f801261326c
deppep <peppedilillo@gmail.com>
2025-03-10 20:05:56 +0100

    minor: favicon not found fix

--- django/hlink/main/urls.py
+++ django/hlink/main/urls.py
@@ -1,8 +1,11 @@
+from django.contrib.staticfiles.storage import staticfiles_storage
 from django.urls import path
+from django.views.generic import RedirectView
 
 from . import views
 
 app_name = "main"
 urlpatterns = [
     path("", views.index, name="index"),
+    path("favicon.ico", RedirectView.as_view(url=staticfiles_storage.url('pics/favicon.ico')))
 ]
