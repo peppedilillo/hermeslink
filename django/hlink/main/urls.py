73c54293dec4d07d4306a6231e8d7e835af17a4f
deppep <peppedilillo@gmail.com>
2025-03-13 17:19:38 +0100

    linter

--- django/hlink/main/urls.py
+++ django/hlink/main/urls.py
@@ -1,12 +1,12 @@
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django.urls import path
 from django.views.generic import RedirectView
-
-from main.views import index, auth_status
+from main.views import auth_status
+from main.views import index
 
 app_name = "main"
 urlpatterns = [
     path("", index, name="index"),
-    path('auth-status/', auth_status, name='auth_status'),
+    path("auth-status/", auth_status, name="auth_status"),
     path("favicon.ico", RedirectView.as_view(url=staticfiles_storage.url("pics/favicon.ico"))),
 ]
