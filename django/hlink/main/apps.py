b24ce115bacbb667f59ddec3e5ba77247f7eb457
peppedilillo <peppedilillo@gmail.com>
2025-03-14 11:05:12 +0100

    hello management command

--- django/hlink/main/apps.py
+++ django/hlink/main/apps.py
@@ -8,8 +8,3 @@ logger = logging.getLogger("hlink")
 class MainConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "main"
-
-    def ready(self):
-        import accounts.signals  # noqa
-
-        logger.info("HL is alive. Godspeed HERMES!!")
---
+++ django/hlink/main/management/commands/say_hi.py
@@ -0,0 +1,11 @@
+from django.core.management.base import BaseCommand
+import logging
+
+logger = logging.getLogger("hlink")
+
+
+class Command(BaseCommand):
+    help = "Setup periodic tasks"
+
+    def handle(self, *args, **kwargs):
+        logger.info("I'm alive. Let's fucking go HERMES!!")
